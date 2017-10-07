6c1ba83b7f7652acfaac9534221ec1e703ac2a9c
Thomas Bénéteau <thomas@bitwise.me>
2017-10-07 14:39:08 +1300

    Make sure to check files exist before attempting to read them

--- modules/gamedev/blog.py
+++ modules/gamedev/blog.py
@@ -16,13 +16,16 @@ class Blog(glados.Module):
 
     @glados.Module.command('blogpost', '', 'Submits the current conversation to the chat blog')
     async def blogpost(self, message, channel):
-        with codecs.open(self.memory['count file'], 'r', encoding='utf-8') as f:
-          contents = f.read()
+        if os.path.isfile(self.memory['count file']):
+            with codecs.open(self.memory['count file'], 'r', encoding='utf-8') as f:
+              contents = f.read()
 
-          if len(contents) == 0:
-              count = 1
-          else:
-              count = int(contents) + 1
+              if len(contents) == 0:
+                  count = 1
+              else:
+                  count = int(contents) + 1
+        else:
+            count = 1
 
         with codecs.open(self.memory['count file'], 'w', encoding='utf-8') as f:
           f.write(count)
@@ -36,16 +39,22 @@ class Blog(glados.Module):
     @glados.Module.command('blogadmin', '<command>', 'Performs admin operations on the chat blog submission queue')
     async def blogadmin(self, message, command):
         if command == 'list':
-            with codecs.open(self.memory['data file'], 'r', encoding='utf-8') as f:
-                submissions = f.read().splitlines()
+            if os.path.isfile(self.memory['data file']):
+                with codecs.open(self.memory['data file'], 'r', encoding='utf-8') as f:
+                    submissions = f.read().splitlines()
+            else:
+                submissions = []
 
             if len(submissions) == 0:
                 await self.client.send_message(message.channel, "No blog submissions to review.")
             else:
                 await self.client.send_message(message.channel, submissions.join("\n"))
         elif command == 'pop':
-            with codecs.open(self.memory['data file'], 'r', encoding='utf-8') as f:
-                submissions = f.read().splitlines()
+            if os.path.isfile(self.memory['data file']):
+                with codecs.open(self.memory['data file'], 'r', encoding='utf-8') as f:
+                    submissions = f.read().splitlines()
+            else:
+                submissions = []
 
             if len(submissions) == 0:
                 await self.client.send_message(message.channel, "No blog submissions to review.")
